85adc5ea22190ca37b205be97a16cbfb06d6242b
Brandon VanBuskirk <Brandonvb@hotmail.ca>
2021-09-27 13:09:45 -0300

    Update Program.cs

--- Methods2/Program.cs
+++ Methods2/Program.cs
@@ -8,8 +8,38 @@ namespace Methods2
 {
     class Program
     {
+        //declaration
+        static int score;
+        static int health;
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("Health = " + health);
+        }
+
+        static void TakeDamage()
+        {
+            health = health - 25;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods2");
+
+            //initialization
+
+            score = 0;
+            health = 100;
+
+            ShowHUD();
+            TakeDamage(10);
+            ShowHUD();
+            TakeDamage(25);
+            ShowHUD();
+
+
+            //missed most of it cause this man doesnt like to show more then 1/3 of his screen at a time.. horrible teacher..
+
+
         }
     }
 }
